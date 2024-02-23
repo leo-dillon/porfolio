817ca0863ddf307273b8d76f39dfc73f913b3b12
Leonardo Dillon <jeannotegui@gmail.com>
2024-02-23 14:47:13 -0300

    Agregando las tecnologias y su responsive

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,11 +3,25 @@ const openMenu = document.querySelector('.open_menu')
 const closeMenu = document.querySelector('.close_menu')
 const nav_vertical = document.querySelector('.nav_vertical')
 const link_vertical = document.querySelectorAll('.link_vertical')
+const tecnologias_iconos = document.querySelector('.tecnologias_iconos')
 
+fetch('./../JSON/tecnologias.json')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.tecnologias)
+        data.tecnologias.forEach(icono => {
+            tecnologias_iconos.innerHTML += `
+            <picture title="Icono de ${icono.name}">
+                <img src="${icono.url}" alt="Icono de ${icono.name}">
+            </picture>
+            `
+        })
+    })
 
+// Reducir header al bajar
 window.addEventListener('scroll', () => {
     if(window.innerWidth > 500){
-        header.classList.toggle('active', window.scrollY > 0)
+        header.classList.toggle('active', window.scrollY > 70)
     }
 })
 link_vertical.forEach(link => {
